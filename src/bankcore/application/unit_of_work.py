b607e07db174f9ec290ce81a153f524b4ae70451
Ange AWALA <ange.awala.bj@gmail.com>
2023-02-28 11:00:00 +0100

    feat(repository): add UnitOfWork + TransferOperation
    - Dirty tracking, identity map, context manager
    - TransferOperation: atomic — both accounts dirty together

---
+++ src/bankcore/application/unit_of_work.py
@@ -0,0 +1,199 @@
+"""
+BankCore — Day 14: Unit of Work
+=================================
+Coordinates atomic operations across multiple repositories.
+
+Problem: a bank transfer modifies TWO accounts. Without UnitOfWork:
+  1. alice.withdraw(500)  → saved
+  2. bob.deposit(500)    → save FAILS
+  → Database has alice's debit but not bob's credit. Inconsistency.
+
+With UnitOfWork:
+  1. alice.withdraw(500)  → tracked (not yet saved)
+  2. bob.deposit(500)    → tracked (not yet saved)
+  3. uow.commit()         → saves BOTH atomically, or neither
+
+This is the application-level equivalent of a database transaction.
+
+Design:
+  - Tracks "dirty" accounts (modified since last commit)
+  - commit() saves all dirty accounts in one operation
+  - rollback() discards all pending changes
+  - Context manager protocol: __enter__ / __exit__
+
+Connection to Day 13 (Schema):
+  The SQLite UnitOfWork uses BEGIN/COMMIT/ROLLBACK at the connection level,
+  making the atomicity a database guarantee, not just an application convention.
+"""
+
+from __future__ import annotations
+from typing import Optional, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from bankcore.application.ports import AccountRepositoryPort
+
+
+class UnitOfWork:
+    """
+    Tracks changes to accounts and commits them atomically.
+
+    Usage:
+        with UnitOfWork(repo) as uow:
+            alice = uow.find("ACC-001")
+            bob   = uow.find("ACC-002")
+            alice.withdraw(500)
+            bob.deposit(500)
+            uow.commit()
+        # On exception: automatic rollback (nothing saved)
+    """
+
+    def __init__(self, repository: "AccountRepositoryPort") -> None:
+        self._repo    = repository
+        self._dirty:  dict = {}   # account_id → account (modified)
+        self._loaded: dict = {}   # account_id → account (original snapshot)
+        self._committed = False
+
+    # ------------------------------------------------------------------
+    # Account access
+    # ------------------------------------------------------------------
+
+    def find(self, account_id: str):
+        """
+        Load an account and register it for change tracking.
+        Returns the same object instance on repeated calls (identity map).
+        """
+        if account_id in self._loaded:
+            return self._loaded[account_id]
+
+        account = self._repo.find_by_id(account_id)
+        if account is not None:
+            self._loaded[account_id] = account
+        return account
+
+    def register_new(self, account) -> None:
+        """Register a newly created account (not yet in the repository)."""
+        self._dirty[account.account_id] = account
+
+    def register_dirty(self, account) -> None:
+        """Mark an account as modified — will be saved on commit."""
+        self._dirty[account.account_id] = account
+
+    # ------------------------------------------------------------------
+    # Commit / Rollback
+    # ------------------------------------------------------------------
+
+    def commit(self) -> None:
+        """
+        Persist all dirty accounts.
+        For SQLite repositories, this also commits the database transaction.
+        """
+        for account in self._dirty.values():
+            self._repo.save(account)
+
+        # If repository supports explicit commit (SQLite), call it
+        if hasattr(self._repo, "commit"):
+            self._repo.commit()
+
+        self._dirty.clear()
+        self._committed = True
+
+    def rollback(self) -> None:
+        """
+        Discard all pending changes.
+        Modified accounts in memory revert to their loaded state.
+        """
+        self._dirty.clear()
+        self._loaded.clear()
+
+        if hasattr(self._repo, "rollback"):
+            self._repo.rollback()
+
+    # ------------------------------------------------------------------
+    # Context manager
+    # ------------------------------------------------------------------
+
+    def __enter__(self) -> "UnitOfWork":
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> bool:
+        if exc_type is None:
+            # No exception — commit if not already done
+            if not self._committed and self._dirty:
+                self.commit()
+        else:
+            # Exception occurred — rollback
+            self.rollback()
+        return False   # do not suppress exceptions
+
+    # ------------------------------------------------------------------
+    # Inspection
+    # ------------------------------------------------------------------
+
+    @property
+    def dirty_count(self) -> int:
+        return len(self._dirty)
+
+    @property
+    def loaded_count(self) -> int:
+        return len(self._loaded)
+
+    def is_dirty(self, account_id: str) -> bool:
+        return account_id in self._dirty
+
+
+class TransferOperation:
+    """
+    Domain operation: transfer funds between two accounts atomically.
+
+    Encapsulates the business logic of a transfer within a UnitOfWork.
+    Used by TransferUseCase when atomic consistency is required.
+
+    This replaces the manual withdraw/deposit sequence in TransactionService
+    for the case where two accounts must be saved together.
+    """
+
+    def __init__(self, uow: UnitOfWork) -> None:
+        self._uow = uow
+
+    def execute(
+        self,
+        from_account_id: str,
+        to_account_id: str,
+        amount: float,
+        description: str = "Transfer",
+    ) -> dict:
+        """
+        Execute a transfer within the Unit of Work.
+        Both accounts are registered as dirty — commit() saves both.
+        """
+        from_account = self._uow.find(from_account_id)
+        to_account   = self._uow.find(to_account_id)
+
+        if from_account is None:
+            return {"success": False, "reason": f"Account {from_account_id} not found."}
+        if to_account is None:
+            return {"success": False, "reason": f"Account {to_account_id} not found."}
+
+        withdrawn = from_account.withdraw(
+            amount, description=f"Transfer to {to_account_id}"
+        )
+        if not withdrawn:
+            return {
+                "success": False,
+                "reason": "Withdrawal refused (insufficient funds or account rules).",
+            }
+
+        to_account.deposit(amount, description=f"Transfer from {from_account_id}")
+
+        # Register both as dirty
+        self._uow.register_dirty(from_account)
+        self._uow.register_dirty(to_account)
+
+        return {
+            "success":           True,
+            "from_account_id":   from_account_id,
+            "to_account_id":     to_account_id,
+            "amount":            amount,
+            "from_balance_after": from_account.balance,
+            "to_balance_after":  to_account.balance,
+        }
